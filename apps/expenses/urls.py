1e1a5c2e1caf1b0691deea4d3115c6a73db71b73
artemkka <artiom.preatca@ebs-integrator.com>
2022-12-05 16:37:27 +0200

    remove trailing_slash

--- apps/expenses/urls.py
+++ apps/expenses/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import ExpenseViewSet
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 
 router.register(r"", ExpenseViewSet, basename="expenses")
 
