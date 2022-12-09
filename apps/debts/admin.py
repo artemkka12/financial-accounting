aae60c000d676cca52764a2b618e06fa51f26ad9
artemkka <artiom.preatca@ebs-integrator.com>
2022-12-09 16:21:08 +0200

    add debts endpoints

--- apps/debts/admin.py
+++ apps/debts/admin.py
@@ -5,4 +5,18 @@ from .models import Debt
 
 @admin.register(Debt)
 class DebtAdmin(admin.ModelAdmin):
-    list_display = ("id", "amount", "currency", "created_at", "updated_at")
+    list_display = [
+        "id",
+        "person",
+        "second_person",
+        "amount",
+        "currency",
+        "type",
+        "is_paid",
+        "deadline",
+        "partial_paid",
+        "partial_paid_amount",
+    ]
+    list_filter = ["person", "second_person", "is_paid", "partial_paid"]
+    search_fields = ["person", "second_person"]
+    ordering = ["-created_at"]
--- apps/debts/models.py
+++ apps/debts/models.py
@@ -9,14 +9,30 @@ class Debt(BaseModel):
         BORROW = "Borrow", "Borrow"
         LEND = "Lend", "Lend"
 
-    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     currency = models.CharField(choices=Currency.choices, max_length=3, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    partial_paid = models.BooleanField(default=False)
+    partial_paid_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     is_paid = models.BooleanField(default=False)
     type = models.CharField(choices=DebtType.choices, max_length=32, null=True, blank=True)
     person = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True)
     second_person = models.CharField(max_length=254, null=True, blank=True)
-    deadline = models.DateTimeField(null=True, blank=True)
+    deadline = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.person} - {self.amount}"
+
+    @property
+    def partial_paid_amount_percent(self):
+        return self.partial_paid_amount / self.amount * 100
+
+    def partial_pay(self, amount):
+        self.partial_paid = True
+        self.partial_paid_amount += amount
+
+        if self.partial_paid_amount >= self.amount:
+            self.is_paid = True
+            self.partial_paid_amount = self.amount
+
+        self.save(update_fields=["partial_paid", "partial_paid_amount", "is_paid"])
