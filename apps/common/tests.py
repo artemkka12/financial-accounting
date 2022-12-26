d2c98c8e0eec5fe87b2036b430a7df006d525f2f
Preatca <artiom.preatca@ebs-integrator.com>
2022-12-26 16:37:24 +0200

    fix auth for test

--- apps/common/tests.py
+++ apps/common/tests.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
 from faker import Faker
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 from ..users.models import User
@@ -10,8 +11,17 @@ fake = Faker()
 class CustomAPITestCase(APITestCase):
     def auth(self):
         username = fake.email()
-        password = fake.password()
-        self.user = User.objects.create(username=username, email=username, password=password)
-        jwt_fetch_data = {"username": username, "password": password}
-        response = self.client.post(reverse("token_obtain_pair"), jwt_fetch_data, "json")
+        data = {
+            "email": username,
+            "username": username,
+            "password": fake.password(),
+        }
+
+        response = self.client.post(reverse("users-list"), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.post(reverse("token_obtain_pair"), data, "json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {response.data['access']}")
+        self.user = User.objects.get(username=username)
