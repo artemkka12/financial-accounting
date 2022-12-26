5230e8329f6b0be0d4e9c3c1a06c3264dab9ec4a
Preatca <artiom.preatca@ebs-integrator.com>
2022-12-26 12:10:36 +0200

    fix test

--- apps/common/tests.py
+++ apps/common/tests.py
@@ -11,7 +11,7 @@ class CustomAPITestCase(APITestCase):
     def auth(self):
         username = fake.email()
         password = fake.password()
-        self.user = User.objects.create_user(username=username, email=username, password=password)
+        self.user = User.objects.create(username=username, email=username, password=password)
         jwt_fetch_data = {"username": username, "password": password}
         response = self.client.post(reverse("token_obtain_pair"), jwt_fetch_data, "json")
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {response.data['access']}")
